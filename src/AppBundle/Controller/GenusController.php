a87d10ed84a63a13dbe5594815c81796f2a90fef
Hector <rootdevelopper@gmail.com>
2018-05-20 10:08:54 -0500

    added Notes action to return notes and data

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -1,42 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hceudev
- * Date: 5/20/18
- * Time: 2:13 AM
- */
 
 namespace AppBundle\Controller;
 
-
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController extends AbstractController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
-
-//    wild card {genusName} should match the parameter name $genusName
-
     public function showAction($genusName)
     {
-        // Symfony v 3 changed - use Abstract Controller instead of Controller
-
-        $notes = [
-            'I saw an Octopus',
-            'Octopus offered Octupi',
-            'Raspberry Pi I said',
-            'Octor RPI'
-        ];
-
         return $this->render('genus/show.html.twig', array(
             'name' => $genusName,
-            'notes' => $notes
         ));
+    }
 
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($genusName)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
     }
 }
